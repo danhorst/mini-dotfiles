a07afdec4b9d3bb51ec84f8669fa61ec8d658315
Dan Brubaker Horst <dan.brubaker.horst@gmail.com>
2026-06-08 14:21:21 -0400

    Explicitly allow homebrew recipes from other taps

---
+++ trust-no-one.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+brew trust --formula danhorst/tap/after-midnight
+brew trust --formula danhorst/tap/gemkeeper
+brew trust --formula danhorst/tap/md-tools
+brew trust --formula danhorst/tap/wrk
+brew trust --formula xwmx/taps/pb
