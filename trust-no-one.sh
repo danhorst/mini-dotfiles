e81ebb59d96ccb8b9d44380b0321364de7e1fd01
Dan Brubaker Horst <dan.brubaker.horst@gmail.com>
2026-07-14 19:56:24 -0400

    Add new utility for adversarial review

--- trust-no-one.sh
+++ trust-no-one.sh
@@ -3,5 +3,6 @@
 brew trust --formula danhorst/tap/after-midnight
 brew trust --formula danhorst/tap/gemkeeper
 brew trust --formula danhorst/tap/md-tools
+brew trust --formula danhorst/tap/second-opinion
 brew trust --formula danhorst/tap/wrk
 brew trust --formula xwmx/taps/pb
